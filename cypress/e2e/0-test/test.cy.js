ecc5d723a87324631c423cdfcab481c17f48059d
Kunjanaphorn <kunjanaphorn.b@gmail.com>
2022-12-22 11:59:57 +0700

    init one

---
+++ cypress/e2e/0-test/test.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("example", () => {
+  beforeEach(() => {
+    cy.visit("https://hec-paris.blackboard.com/ultra/course"); //add url
+  });
+
+  it("Login", () => {
+    const username = "saraphan.choowattanapakorn@hec.edu"; //add username
+    const password = "Arthur6235sarah$"; //add password
+    cy.get("#userNameInput")
+      .type(username, { delay: 100 })
+      .should("have.value", username);
+    cy.get("#nextButton").click();
+    cy.get("#passwordInput")
+      .type(password, { delay: 100 })
+      .should("have.value", password);
+    cy.get("#submitButton").click();
+    cy.on("uncaught:exception", (e) => {
+      if (e.message.includes("Things went bad")) {
+        // we expected this error, so let's ignore it
+        // and let the test continue
+        return false;
+      }
+    });
+    cy.visit("https://hec-paris.blackboard.com/auth-saml/saml/redirectToLearn");
+  });
+});
